053ec74360b125b0413c01308e1e76a62a1d8ed7
David Palacios <dboxing22@gmail.com>
2021-11-01 14:50:18 -0500

    update

--- hello/firstTask.py
+++ hello/firstTask.py
@@ -156,78 +156,7 @@ def most_new_students() :
 # print(students_assigned_new_school("TCA Head Start at Mahalia Jackson (Head Start)"))
 # print(students_assigned_new_school("Élan Academy"))
 
-"Akili Academy of New Orleans"	# 1
-"Andrew H. Wilson Charter School"	# 2
-"Audubon Charter School French"	# 3
-"Audubon Gentilly"	# 1
-"Audubon Gentilly Tuition"	# 1
-"Benjamin Franklin Elementary Mathematics and Science School"	# 3
-"Booker T. Washington High School KIPP)"	# 2
-"Bricolage Academy"	# 7
-"CA: G.W. Carver High School"	# 2
-"CA: Livingston Collegiate Academy"	# 3
-"CA: Rosenwald Collegiate Academy"	# 1
-"Carrollton-Dunbar Head Start Head Start)"	# 1
-"Delores Taylor Arthur School for Young Men"	# 1
-"Dr. Martin Luther King Jr. Charter School"	# 7
-"Dr. Peter Dangerfield Head Start Head Start)"	# 2
-"Dwight D. Eisenhower Academy of Global Studies"	# 3
-"Edna Karr High School"	# 4
-"Educare Head Start)"	# 1
-"Edward Hynes Charter School - Lakeview Campus"	# 1
-"Einstein Charter Middle School at Sarah Towles Reed"	# 0
-"Eleanor McMain Secondary School"	# 1
-"Encore Academy"	# 1
-"Esperanza Charter School"	# 3
-"FirstLine Schools: Arthur Ashe Charter School"	# 3
-"FirstLine Schools: Langston Hughes Academy"	# 1
-"FirstLine Schools: Live Oak Charter School"	# 2
-"FirstLine Schools: Phillis Wheatley Community School"	# 5
-"Foundation Preparatory Charter"	# 1
-"Frederick A. Douglass High School KIPP Renaissance)"	# 1
-"Good Shepherd Nativity Mission School"	# 2
-"Harriet Tubman Charter School"	# 5
-"Homer A. Plessy Community School"	# 2
-"IDEA Oscar Dunn"	# 1
-"International High School of New Orleans"	# 1
-"International School of Louisiana: Dixon Campus French)"	# 4
-"International School of Louisiana: Dixon Campus Spanish)"	# 2
-"James M. Singleton Charter School"	# 4
-"John F. Kennedy High School KIPP)"	# 1
-"KIPP Believe"	# 1
-"KIPP Central City"	# 2
-"KIPP East"	# 4
-"KIPP Leadership"	# 7
-"KIPP Morial"	# 1
-"Kingsley House Incorporated Preschool and School Age Daycare) Head Start)"	# 2
-"L. B. Landry - O. Perry Walker College and Career Preparatory High School"	# 1
-"Lafayette Academy Charter School"	# 2
-"Lawrence D. Crocker College Prep: A School for the Arts and Technology"	# 1
-"Living School"	# 1
-"Lycee Francais de la Nouvelle-Orleans LFNO)"	# 9
-"Lycee Francais de la Nouvelle-Orleans LFNO) Tuition"	# 1
-"Martin Behrman Charter School Academy of Creative Arts and Sciences"	# 6
-"Mary McLeod Bethune Elementary Charter School"	# 2
-"McDonogh #35 Senior High School"	# 2
-"McDonogh #42 Elementary Charter School"	# 2
-"Morris Jeff Community School"	# 11
-"New Harmony High"	# 1
-"New Orleans Charter Science and Math High School Sci High)"	# 3
-"New Orleans Military and Maritime Academy"	# 5
-"Paul Habans Charter School"	# 5
-"Pearlie H. Elloie Head Start Center TCA Head Start)"	# 1
-"Pierre A. Capdau @ Avery Alexander Charter School"	# 1
-"ReNEW Dolores T. Aaron Academy"	# 2
-"Resurrection of Our Lord School"	# 2
-"Robert Russa Moton Charter School"	# 1
-"Sophie B. Wright High School"	# 1
-"St. Joan of Arc School"	# 1
-"St. Leo the Great"	# 1
-"St. Paul the Apostle Head Start)"	# 1
-"St. Rita School"	# 2
-"Success at Thurgood Marshall"	# 2
-"TCA Head Start at Mahalia Jackson Head Start)"	# 1
-"Élan Academy"	# 4
+
 
 
 conn.close()
